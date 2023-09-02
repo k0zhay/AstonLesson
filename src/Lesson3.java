4878c93393dc43335832dbc5a449348ab93d058b
kozha <kozhaevs2000@gmail.com>
2023-09-02 16:51:06 +0300

    Lesson_3

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -3,46 +3,41 @@
 в удобоваримом виде. Надеюсь, получилось.
  */
 
-import java.util.Arrays;
-
 public class Lesson3 {
     public static void main(String[] args) {
         String result = "Результат выполнения задания №";
         // Задание №1:
-        System.out.println(result + "1:\n" + methods.isSumWithin(5, 15) + "\n");
+        System.out.printf(result + "1:\n%b \n\n",
+                methods.isSumWithin(5, 15));
         // Задание №2:
         System.out.println(result + "2:");
         methods.isPositive(0);
         System.out.println();
         // Задание №3:
-        System.out.printf(result + "3:\n%b \n\n", methods.isNegative(-1));
+        System.out.printf(result + "3:\n%b \n\n",
+                methods.isNegative(-1));
         // Задание №4:
         System.out.println(result + "4:");
-        methods.printText("Я дважды повторять не стану!", 5);
+        methods.printText("Я дважды повторять не стану!", 3);
         System.out.println();
         // Задание №5:
-        System.out.printf(result + "5:\n%b \n\n", methods.isLeapYear(2001));
+        System.out.printf(result + "5:\n%b \n\n",
+                methods.isLeapYear(2000));
         // Задание №6:
-        System.out.println(result + "6:");
-        methods.replaceOneZero();
-        System.out.println();
+        System.out.printf(result + "6:\n%s\n\n",
+                methods.replaceOneZero());
         // Задание №7:
-        System.out.println(result + "7:");
-        methods.arrHundred();
-        System.out.println();
+        System.out.printf(result + "7:\n%s\n\n",
+                methods.arrHundred());
         // Задание №8:
-        System.out.println(result + "8:");
-        methods.lessThanSix();
-        System.out.println();
+        System.out.printf(result + "8:\n%s\n\n",
+                methods.lessThanSix());
         // Задание №9:
         System.out.println(result + "9:");
-        methods.fillDiagonal(6);
+        methods.fillDiagonal(5);
         System.out.println();
         // Задание №10:
         System.out.printf(result + "10:\n%s",
-                Arrays.toString(methods.arrCreate(3, 999)));
+                methods.arrCreate(3, 999));
     }
 }
-
-
-
--- src/methods.java
+++ src/methods.java
@@ -62,36 +62,36 @@ public class methods {
     Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
     С помощью цикла и условия заменить 0 на 1, 1 на 0.
      */
-    static void replaceOneZero() {
+    static String replaceOneZero() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (arr[i] == 1) ? 0 : 1;
         }
-        System.out.println(Arrays.toString(arr));
+        return Arrays.toString(arr);
     }
 
     /*
     7. Задать пустой целочисленный массив длиной 100.
     С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100.
     */
-    static void arrHundred() {
+    static String arrHundred() {
         int[] arr = new int[100];
         for (int i = 0; i < 100; i++) {
             arr[i] = i + 1;
         }
-        System.out.println(Arrays.toString(arr));
+        return Arrays.toString(arr);
     }
 
     /*
     8. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ],
     пройти по нему циклом, и числа меньшие 6 умножить на 2.
      */
-    static void lessThanSix() {
+    static String lessThanSix() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] < 6 ? arr[i] * 2 : arr[i];
         }
-        System.out.println(Arrays.toString(arr));
+        return Arrays.toString(arr);
     }
 
     /*
@@ -118,9 +118,9 @@ public class methods {
     и возвращающий одномерный массив типа int длиной len, каждая ячейка
     которого равна initialValue.
      */
-    static int[] arrCreate(int len, int initialValue) {
+    static String arrCreate(int len, int initialValue) {
         int[] arr = new int[len];
         Arrays.fill(arr, initialValue);
-        return arr;
+        return Arrays.toString(arr);
     }
 }
