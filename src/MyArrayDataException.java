6707e91dad042cacf288de5d2d068529e623435c
kozha <kozhaevs2000@gmail.com>
2023-09-19 15:51:18 +0300

    Lesson_8

--- src/MyArrayDataException.java
+++
@@ -1,6 +0,0 @@
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(int row, int column) {
-        System.out.printf("Ошибка: элемент(%d, %d) - не число\n", row, column);
-    }
-}
--- src/MyArraySizeException.java
+++
@@ -1,6 +0,0 @@
-
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException() {
-        System.out.println("Ошибка: размер массива не 4х4");
-    }
-}
